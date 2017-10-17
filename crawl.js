9b44856eccd17d3d6c7d9c8f3b8f54689906842f
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-17 09:19:32 -0700

    refactor to make nice web scraper

--- crawl.js
+++ crawl.js
@@ -1,11 +1,18 @@
 const cheerio = require('cheerio')
 
+const _ = require('lodash')
+
 const fs = require('fs');
 
-const $ = cheerio.load(fs.readFileSync('./hacker-news.html'));
+const document = fs.readFileSync('./hacker-news.html', 'utf-8');
+
+const $ = cheerio.load(document)
 
 
-//console.log($.html())
+const links = Array.from($('a.storylink'))
+const item = _.sample(links)
+const title = $(item).text()
+const link = $(item).attr('href')
+const result = `${title}\n${link}`
 
-
-console.log($('.athing').toArray())
+console.log(result)
